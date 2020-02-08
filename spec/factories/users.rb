1909e83d01f1f9d485c9e529fe58657dc7b1ed34
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-08 19:50:47 +0200

    Refactor user spec to use FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    first_name: "Geo"
-    last_name: "Fra"
-    email: "test@test.com"
-    password: "123456"
+    first_name "Geo"
+    last_name "Fra"
+    email "test@test.com"
+    password "123456"
   end
 end
