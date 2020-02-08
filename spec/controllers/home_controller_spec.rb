5d211987cfc343ab3664071eac64c7d648183c06
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-08 21:54:14 +0200

    Add spec for unsigned user

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe HomeController, type: :controller do
 
     it "returns a 200 response" do
       get :index
-      expect(:response).to have_http_status "200"
+      expect(response).to have_http_status "200"
     end
   end
 end
