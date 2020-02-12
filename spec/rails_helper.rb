0237d84eb9c6a83074cdde482d73d50c3fd5517b
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-12 21:08:52 +0200

    Implement request spec for projects and configuration spec for Devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, type: :request
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
