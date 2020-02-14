ab6c6f72ca3e6c47b804dcd443360fdf6f73be10
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-14 20:09:29 +0200

    Implement aggregate failures

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "Projects", type: :feature do
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
-    
+    # using our customer login helper:
+    # sign_in_as user
+    # or the one provided by Devise:
     sign_in user
 
     visit root_path
@@ -14,9 +16,11 @@ RSpec.feature "Projects", type: :feature do
       fill_in "Description", with: "Trying out Capybara"
       click_button "Create Project"
 
-      expect(page).to have_content "Project was successfully created"
-      expect(page).to have_content "Test Project"
-      expect(page).to have_content "Owner: #{user.name}"
+      aggregate_failures do
+        expect(page).to have_content "Project was successfully created"
+        expect(page).to have_content "Test Project"
+        expect(page).to have_content "Owner: #{user.name}"
+      end
     }.to change(user.projects, :count).by(1)
   end
 end
