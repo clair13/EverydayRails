1f7378b67ea54666771fbb87a9461c12c66acb3a
clair13 <theclairvoyant1313@yahoo.gr>
2020-02-17 19:42:59 +0200

    Implement notes controller spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,7 @@ RSpec.configure do |config|
 end
 
 Shoulda::Matchers.configure do |config|
-  confid.integrate do |with|
+  config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
